46cdb2839cdbebe7a0ea7224b5e7b780474d201d
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-13 21:45:35 +0600

    Add files via upload

---
+++ ES6 Variable, Function, Showing Output/new.js
@@ -0,0 +1,10 @@
+//ES6 variable decleration
+let name = "Sourav Dhali";
+console.log(`My Name is ${name}`);
+
+//ES6 function creation
+const add = (num1,num2) => {
+    console.log(`The value is : ${num1+num2} .`)
+    return num1+num2;
+}
+console.log(add(10,20));
