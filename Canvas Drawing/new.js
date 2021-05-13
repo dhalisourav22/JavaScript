0a2b4fc64855c900018f0707d709cc89165cac2f
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-13 19:33:26 +0600

    Add files via upload

---
+++ Canvas Drawing/new.js
@@ -0,0 +1,36 @@
+var c = document.getElementById("my-canvas");
+var ctx = c.getContext("2d");
+
+//making Rectangle
+ctx.fillStyle = "rgb(132, 132, 223)";
+ctx.fillRect(10,10,352,260); 
+
+//Making Stroke
+ctx.lineWidth=3;
+ctx.strokeStyle = "blue"
+ctx.strokeRect(8,8,356,264);
+
+//making Circle
+ctx.beginPath();
+ctx.fillStyle = "rgb(201, 201, 247)";
+ctx.arc(356/2,264/2,100,0,2*Math.PI,false);
+ctx.fill();
+ctx.stroke();
+
+//making Line 
+ctx.moveTo(10,10);
+ctx.lineTo(363,271);
+ctx.stroke();
+
+//making Text
+ctx.font = "30px Verdana";
+// ctx.fillStyle = "black";
+// ctx.fillText("This is Sourav",10,100);
+ctx.strokeStyle = "black";
+ctx.strokeText("This is Sourav",10,100);
+
+//making Image
+// function imgWork(){
+//     var img = document.querySelector("#my-img");
+//     ctx.drawImage(img,10,10);
+// }
