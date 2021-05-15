4b3386f937c92b741d38156f9e66b899d3d4a657
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-15 11:47:27 +0600

    Update new.js

--- Object Literal/new.js
+++ Object Literal/new.js
@@ -6,13 +6,16 @@ function show(name,age){
     return data;
 }
 console.log(show("sourav",22));
+
+//consize method/syntex
 let message = {
-    display(){
+    display(){         //consize method/syntex
         console.log("This is display function..!");
     }
 }
 message.display();
 
+//making function with space
 let message1 = {
     "display fun"(){
         console.log("This is display function..!");
