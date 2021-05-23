92c606a5eaf9180157a6710df32af413ebbf2a15
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-23 12:47:46 +0600

    Add files via upload

---
+++ Array Of Object/new.js
@@ -0,0 +1,33 @@
+var s1 = {
+    name : "Sourav",
+    cgpa : 3.75
+}
+var s2 = {
+    name : "Juthi",
+    cgpa : 4.00
+}
+var s3 = {
+    name : "Pritesh",
+    cgpa : 3.08
+}
+var student = [s1,s3,s2];
+console.log(student);
+
+
+
+//direct declaration
+var student1 = [{
+    name : "Sourav",
+    cgpa : 3.75
+},
+{
+    name : "Juthi",
+    cgpa : 4.00
+},
+{
+    name : "Pritesh",
+    cgpa : 3.08
+}
+];
+var student1 = [s3,s1,s2];
+console.log(student1);
