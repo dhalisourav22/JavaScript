249b2567c453b982ec6de06fa3677b2ae2d147ad
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-25 21:06:16 +0600

    Add files via upload

---
+++ String Methods/style.js
@@ -0,0 +1,18 @@
+//All string methor is case sensitive. thats means they check on case.
+//This three method always return in boolean. Either it's trur or false.
+
+//startsWith()
+let sentence = "My name dhali";
+console.log(sentence.startsWith("My"));  //string checking
+console.log(sentence.startsWith("M"));   //cherecter checking
+console.log(sentence.startsWith("n",3)); //Using index. this is the starting point of the sentence string.
+
+//endWith()
+console.log(sentence.endsWith("dhali"));  //string checking
+console.log(sentence.endsWith("i"));      //cherecter checking
+console.log(sentence.endsWith("i",13));   //Using index. this is the ending point of the sentence string.
+
+//include()
+console.log(sentence.includes("name"));   //string checking
+console.log(sentence.includes("m"));      //cherecter checking
+console.log(sentence.includes("m",5));    //using index. like 2 step varification process.
