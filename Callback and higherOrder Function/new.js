a2eb7b6ee8d1c586b2fd139f5f5ad0c52a43e38a
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-28 09:56:01 +0600

    Add files via upload

---
+++ Callback and higherOrder Function/new.js
@@ -0,0 +1,14 @@
+/**
+ * this program is showing callback fuction and higher order function
+ * we can customized the name of callback function as our wish
+ */
+
+function square (x){
+    console.log(`The square of ${x} : ${x*x}`);
+}
+
+function higherOrderFunction (num, callback){ //the pasing function as a parameter is called callback function
+    callback(num);     //tht's how this is call a previous function. thst's why this is called a callback function
+}
+
+higherOrderFunction(12, square);   //the function which take an parameter as a function , this function is called a higher order function
