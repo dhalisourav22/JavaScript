073fbf97742ba2d9f62c34bf259d1f9635d60322
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-06-29 10:33:28 +0600

    Update new.js

--- Acync and Await/new.js
+++ Acync and Await/new.js
@@ -24,24 +24,24 @@ const taskFour =() =>{
 };
 
 
-const callingAllTask = async () =>{      //Thats the arrow function
+const callingAllTask = async () =>{      //Thats the arrow function.
     try{
         const t1 = await taskOne();
         console.log(t1);
         const t2 = await taskTwo();
         console.log(t2);
-        const t3 = await taskThree();    //showinng error but the catch block will handle this error
+        const t3 = await taskThree();    //showinng error but the catch block will handle this error.
         console.log(t3);
         const t4 = await taskFour();
         console.log(t4);
     }catch(err){
-        console.log(err);                //This is recive the error of taskThree
+        console.log(err);                //This is recive the error of taskThree.
     }
 }
 callingAllTask();
 
 
-async function showingAllTask(){         //This is treditional function
+async function showingAllTask(){         //This is treditional function.
     const t1 = await taskOne();
     console.log(t1);
     const t2 = await taskTwo();
